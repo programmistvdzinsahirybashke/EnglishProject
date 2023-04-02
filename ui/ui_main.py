1ce4fdc0385382177a3186bb733ec3e70158b772
programmistvdzinsahirybashke <ilnaz09.ys@gmail.com>
2023-04-02 10:44:32 +0300

    sadfasdf

---
+++ ui/ui_main.py
@@ -0,0 +1,208 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'ui_main.ui'
+##
+## Created by: Qt User Interface Compiler version 6.4.3
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QFrame, QLabel, QMainWindow,
+    QPushButton, QSizePolicy, QWidget)
+
+class Ui_StartWindow(object):
+    def setupUi(self, StartWindow):
+        if not StartWindow.objectName():
+            StartWindow.setObjectName(u"StartWindow")
+        StartWindow.resize(540, 516)
+        font = QFont()
+        font.setStrikeOut(False)
+        StartWindow.setFont(font)
+        self.centralwidget = QWidget(StartWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(60, 30, 431, 81))
+        font1 = QFont()
+        font1.setFamilies([u"Montserrat ExtraBold"])
+        font1.setPointSize(60)
+        font1.setBold(True)
+        font1.setItalic(False)
+        font1.setStrikeOut(False)
+        font1.setStyleStrategy(QFont.PreferAntialias)
+        self.label.setFont(font1)
+        self.label.setStyleSheet(u"QLabel {  color : rgb(0, 170, 127); }")
+        self.frame = QFrame(self.centralwidget)
+        self.frame.setObjectName(u"frame")
+        self.frame.setGeometry(QRect(0, 0, 541, 517))
+        self.frame.setStyleSheet(u"border: 9px solid #66CDAA;\n"
+"")
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.SettingsButton = QPushButton(self.frame)
+        self.SettingsButton.setObjectName(u"SettingsButton")
+        self.SettingsButton.setGeometry(QRect(40, 430, 150, 50))
+        palette = QPalette()
+        brush = QBrush(QColor(255, 255, 254, 255))
+        brush.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.WindowText, brush)
+        brush1 = QBrush(QColor(44, 182, 125, 255))
+        brush1.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.Button, brush1)
+        palette.setBrush(QPalette.Active, QPalette.Text, brush)
+        palette.setBrush(QPalette.Active, QPalette.ButtonText, brush)
+        palette.setBrush(QPalette.Active, QPalette.Base, brush1)
+        palette.setBrush(QPalette.Active, QPalette.Window, brush1)
+        brush2 = QBrush(QColor(255, 255, 254, 128))
+        brush2.setStyle(Qt.SolidPattern)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
+        palette.setBrush(QPalette.Active, QPalette.PlaceholderText, brush2)
+#endif
+        palette.setBrush(QPalette.Inactive, QPalette.WindowText, brush)
+        palette.setBrush(QPalette.Inactive, QPalette.Button, brush1)
+        palette.setBrush(QPalette.Inactive, QPalette.Text, brush)
+        palette.setBrush(QPalette.Inactive, QPalette.ButtonText, brush)
+        palette.setBrush(QPalette.Inactive, QPalette.Base, brush1)
+        palette.setBrush(QPalette.Inactive, QPalette.Window, brush1)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
+        palette.setBrush(QPalette.Inactive, QPalette.PlaceholderText, brush2)
+#endif
+        palette.setBrush(QPalette.Disabled, QPalette.WindowText, brush)
+        palette.setBrush(QPalette.Disabled, QPalette.Button, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.Text, brush)
+        palette.setBrush(QPalette.Disabled, QPalette.ButtonText, brush)
+        palette.setBrush(QPalette.Disabled, QPalette.Base, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.Window, brush1)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
+        palette.setBrush(QPalette.Disabled, QPalette.PlaceholderText, brush2)
+#endif
+        self.SettingsButton.setPalette(palette)
+        font2 = QFont()
+        font2.setFamilies([u"Montserrat"])
+        font2.setBold(True)
+        font2.setItalic(False)
+        font2.setStrikeOut(False)
+        font2.setKerning(True)
+        font2.setStyleStrategy(QFont.PreferAntialias)
+        self.SettingsButton.setFont(font2)
+        self.SettingsButton.setStyleSheet(u"QPushButton {\n"
+"background: #2CB67D;\n"
+"border: 4px solid #66CDAA;\n"
+"border-radius: 21px;\n"
+"    padding: 5px;\n"
+"    }\n"
+"\n"
+"QPushButton:hover {\n"
+"    background: #66CDAB;\n"
+"    }\n"
+"\n"
+"QPushButton:pressed {\n"
+"    background: #2CB67D;\n"
+"    }\n"
+"\n"
+"QPushButton\n"
+"{\n"
+" font-family: 'Montserrat';\n"
+"font-style: normal;\n"
+"font-weight: 600;\n"
+"font-size: 22px;\n"
+"line-height: 29px;\n"
+"text-align: center;\n"
+"\n"
+"color: #FFFFFE;\n"
+"}")
+        self.StartButton = QPushButton(self.frame)
+        self.StartButton.setObjectName(u"StartButton")
+        self.StartButton.setGeometry(QRect(200, 430, 150, 50))
+        self.StartButton.setFont(font2)
+        self.StartButton.setAcceptDrops(False)
+        self.StartButton.setStyleSheet(u"QPushButton {\n"
+"background: #2CB67D;\n"
+"border: 4px solid #66CDAA;\n"
+"border-radius: 21px;\n"
+"    padding: 5px;\n"
+"    }\n"
+"\n"
+"QPushButton:hover {\n"
+"    background: #66CDAB;\n"
+"    }\n"
+"\n"
+"QPushButton:pressed {\n"
+"    background: #2CB67D;\n"
+"    }\n"
+"\n"
+"QPushButton\n"
+"{\n"
+" font-family: 'Montserrat';\n"
+"font-style: normal;\n"
+"font-weight: 600;\n"
+"font-size: 26px;\n"
+"line-height: 29px;\n"
+"text-align: center;\n"
+"\n"
+"color: #FFFFFE;\n"
+"}")
+        self.StartButton.setAutoDefault(False)
+        self.ExitButton = QPushButton(self.frame)
+        self.ExitButton.setObjectName(u"ExitButton")
+        self.ExitButton.setGeometry(QRect(360, 430, 150, 50))
+        self.ExitButton.setFont(font2)
+        self.ExitButton.setAcceptDrops(False)
+        self.ExitButton.setStyleSheet(u"QPushButton {\n"
+"background: #2CB67D;\n"
+"border: 4px solid #66CDAA;\n"
+"border-radius: 21px;\n"
+"    padding: 5px;\n"
+"    }\n"
+"\n"
+"QPushButton:hover {\n"
+"    background: #66CDAB;\n"
+"    }\n"
+"\n"
+"QPushButton:pressed {\n"
+"    background: #2CB67D;\n"
+"    }\n"
+"\n"
+"QPushButton\n"
+"{\n"
+" font-family: 'Montserrat';\n"
+"font-style: normal;\n"
+"font-weight: 600;\n"
+"font-size: 22px;\n"
+"line-height: 29px;\n"
+"text-align: center;\n"
+"\n"
+"color: #FFFFFE;\n"
+"}")
+        self.ExitButton.setAutoDefault(False)
+        self.label_2 = QLabel(self.centralwidget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(25, 120, 489, 284))
+        self.label_2.setScaledContents(False)
+        StartWindow.setCentralWidget(self.centralwidget)
+        self.frame.raise_()
+        self.label.raise_()
+        self.label_2.raise_()
+
+        self.retranslateUi(StartWindow)
+
+        QMetaObject.connectSlotsByName(StartWindow)
+    # setupUi
+
+    def retranslateUi(self, StartWindow):
+        StartWindow.setWindowTitle(QCoreApplication.translate("StartWindow", u"Crocodile!", None))
+        self.label.setText(QCoreApplication.translate("StartWindow", u"Crocodile!", None))
+        self.SettingsButton.setText(QCoreApplication.translate("StartWindow", u"Settings", None))
+        self.StartButton.setText(QCoreApplication.translate("StartWindow", u"Start!", None))
+        self.ExitButton.setText(QCoreApplication.translate("StartWindow", u"Exit", None))
+        self.label_2.setText("")
+    # retranslateUi
+
--- ui_settings.py
+++ ui_settings.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'ui_settings.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.4.2
+## Created by: Qt User Interface Compiler version 6.4.3
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -22,23 +22,34 @@ class Ui_SettingsWindow(object):
     def setupUi(self, SettingsWindow):
         if not SettingsWindow.objectName():
             SettingsWindow.setObjectName(u"SettingsWindow")
-        SettingsWindow.resize(540, 600)
+        SettingsWindow.resize(540, 516)
+        SettingsWindow.setMinimumSize(QSize(540, 516))
+        SettingsWindow.setMaximumSize(QSize(540, 516))
         font = QFont()
         font.setFamilies([u"Montserrat"])
         SettingsWindow.setFont(font)
         self.S = QLabel(SettingsWindow)
         self.S.setObjectName(u"S")
-        self.S.setGeometry(QRect(140, 90, 271, 81))
+        self.S.setGeometry(QRect(50, 10, 451, 121))
         font1 = QFont()
         font1.setFamilies([u"Montserrat"])
-        font1.setPointSize(44)
         font1.setBold(True)
         font1.setItalic(False)
         self.S.setFont(font1)
-        self.S.setStyleSheet(u"QLabel {  color : rgb(0, 170, 127); }")
+        self.S.setStyleSheet(u"QLabel {  font-family: 'Montserrat';\n"
+"font-style: normal;\n"
+"font-weight: 700;\n"
+"font-size: 90px;\n"
+"line-height: 150%;\n"
+"/* or 135px */\n"
+"\n"
+"text-align: center;\n"
+"letter-spacing: -0.022em;\n"
+"\n"
+"color: #2CB67D; }")
         self.label = QLabel(SettingsWindow)
         self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(60, 240, 171, 41))
+        self.label.setGeometry(QRect(60, 160, 171, 41))
         font2 = QFont()
         font2.setFamilies([u"Montserrat"])
         font2.setPointSize(28)
@@ -47,42 +58,13 @@ class Ui_SettingsWindow(object):
         self.label.setFont(font2)
         self.label.setStyleSheet(u"QLabel {  color : rgb(0, 170, 127); }")
         self.label.setMidLineWidth(1)
-        self.Back = QPushButton(SettingsWindow)
-        self.Back.setObjectName(u"Back")
-        self.Back.setGeometry(QRect(60, 440, 210, 60))
-        font3 = QFont()
-        font3.setFamilies([u"Montserrat Medium"])
-        font3.setPointSize(16)
-        self.Back.setFont(font3)
-        self.Back.setStyleSheet(u"QPushButton {\n"
-"    color: #333;\n"
-"    border: 2px solid white;\n"
-"    border-radius: 30px;\n"
-"    border-style: outset;\n"
-"    background: rgb(102,205,170);\n"
-"    padding: 5px;\n"
-"    }\n"
-"\n"
-"QPushButton:hover {\n"
-"    background: rgb(3, 205, 150);\n"
-"    }\n"
-"\n"
-"QPushButton:pressed {\n"
-"    border-style: inset;\n"
-"    background: rgb(70, 255, 150);\n"
-"    }\n"
-"\n"
-"QPushButton\n"
-"{\n"
-"   color:white;\n"
-"}")
         self.radioButtonSetGreen = QRadioButton(SettingsWindow)
         self.radioButtonSetGreen.setObjectName(u"radioButtonSetGreen")
-        self.radioButtonSetGreen.setGeometry(QRect(240, 240, 121, 41))
-        font4 = QFont()
-        font4.setFamilies([u"Montserrat"])
-        font4.setPointSize(24)
-        self.radioButtonSetGreen.setFont(font4)
+        self.radioButtonSetGreen.setGeometry(QRect(245, 160, 121, 41))
+        font3 = QFont()
+        font3.setFamilies([u"Montserrat"])
+        font3.setPointSize(24)
+        self.radioButtonSetGreen.setFont(font3)
         self.radioButtonSetGreen.setStyleSheet(u"QRadioButton::indicator::unchecked{ \n"
 "border: 1px solid; \n"
 "border-color: rgb(132,132,132);\n"
@@ -102,8 +84,8 @@ class Ui_SettingsWindow(object):
 "}")
         self.radioButtonSetDark = QRadioButton(SettingsWindow)
         self.radioButtonSetDark.setObjectName(u"radioButtonSetDark")
-        self.radioButtonSetDark.setGeometry(QRect(370, 240, 101, 41))
-        self.radioButtonSetDark.setFont(font4)
+        self.radioButtonSetDark.setGeometry(QRect(375, 160, 101, 41))
+        self.radioButtonSetDark.setFont(font3)
         self.radioButtonSetDark.setStyleSheet(u"QRadioButton::indicator::unchecked{ \n"
 "border: 1px solid; \n"
 "border-color: rgb(132,132,132);\n"
@@ -123,20 +105,20 @@ class Ui_SettingsWindow(object):
 "}")
         self.label_2 = QLabel(SettingsWindow)
         self.label_2.setObjectName(u"label_2")
-        self.label_2.setGeometry(QRect(60, 330, 171, 41))
-        font5 = QFont()
-        font5.setFamilies([u"Montserrat"])
-        font5.setPointSize(28)
-        font5.setBold(True)
-        font5.setItalic(True)
-        font5.setStyleStrategy(QFont.PreferDefault)
-        self.label_2.setFont(font5)
+        self.label_2.setGeometry(QRect(60, 250, 171, 41))
+        font4 = QFont()
+        font4.setFamilies([u"Montserrat"])
+        font4.setPointSize(28)
+        font4.setBold(True)
+        font4.setItalic(True)
+        font4.setStyleStrategy(QFont.PreferDefault)
+        self.label_2.setFont(font4)
         self.label_2.setStyleSheet(u"QLabel {  color : rgb(0, 170, 127); }")
         self.label_2.setMidLineWidth(1)
         self.radioButtonSetTimeMInuteThirty = QRadioButton(SettingsWindow)
         self.radioButtonSetTimeMInuteThirty.setObjectName(u"radioButtonSetTimeMInuteThirty")
-        self.radioButtonSetTimeMInuteThirty.setGeometry(QRect(285, 330, 81, 41))
-        self.radioButtonSetTimeMInuteThirty.setFont(font4)
+        self.radioButtonSetTimeMInuteThirty.setGeometry(QRect(295, 250, 81, 41))
+        self.radioButtonSetTimeMInuteThirty.setFont(font3)
         self.radioButtonSetTimeMInuteThirty.setStyleSheet(u"QRadioButton::indicator::unchecked{ \n"
 "border: 1px solid; \n"
 "border-color: rgb(132,132,132);\n"
@@ -156,8 +138,8 @@ class Ui_SettingsWindow(object):
 "}")
         self.radioButtonSetTimeMInute = QRadioButton(SettingsWindow)
         self.radioButtonSetTimeMInute.setObjectName(u"radioButtonSetTimeMInute")
-        self.radioButtonSetTimeMInute.setGeometry(QRect(190, 330, 81, 41))
-        self.radioButtonSetTimeMInute.setFont(font4)
+        self.radioButtonSetTimeMInute.setGeometry(QRect(205, 250, 81, 41))
+        self.radioButtonSetTimeMInute.setFont(font3)
         self.radioButtonSetTimeMInute.setStyleSheet(u"QRadioButton::indicator::unchecked{ \n"
 "border: 1px solid; \n"
 "border-color: rgb(132,132,132);\n"
@@ -177,22 +159,22 @@ class Ui_SettingsWindow(object):
 "}")
         self.frame_5 = QFrame(SettingsWindow)
         self.frame_5.setObjectName(u"frame_5")
-        self.frame_5.setGeometry(QRect(50, 320, 440, 61))
-        self.frame_5.setStyleSheet(u"    border: 2px solid  rgb(102,205,170);\n"
-"")
+        self.frame_5.setGeometry(QRect(50, 240, 440, 61))
+        self.frame_5.setStyleSheet(u"border: 3px solid #66CDAA;\n"
+"border-radius: 10px;")
         self.frame_5.setFrameShape(QFrame.StyledPanel)
         self.frame_5.setFrameShadow(QFrame.Raised)
         self.frame_7 = QFrame(SettingsWindow)
         self.frame_7.setObjectName(u"frame_7")
-        self.frame_7.setGeometry(QRect(50, 230, 440, 61))
-        self.frame_7.setStyleSheet(u"    border: 2px solid  rgb(102,205,170);\n"
-"")
+        self.frame_7.setGeometry(QRect(50, 150, 440, 61))
+        self.frame_7.setStyleSheet(u"border: 3px solid #66CDAA;\n"
+"border-radius: 10px;")
         self.frame_7.setFrameShape(QFrame.StyledPanel)
         self.frame_7.setFrameShadow(QFrame.Raised)
         self.radioButtonSetTimeTwoMinutes = QRadioButton(SettingsWindow)
         self.radioButtonSetTimeTwoMinutes.setObjectName(u"radioButtonSetTimeTwoMinutes")
-        self.radioButtonSetTimeTwoMinutes.setGeometry(QRect(375, 330, 91, 41))
-        self.radioButtonSetTimeTwoMinutes.setFont(font4)
+        self.radioButtonSetTimeTwoMinutes.setGeometry(QRect(380, 250, 91, 41))
+        self.radioButtonSetTimeTwoMinutes.setFont(font3)
         self.radioButtonSetTimeTwoMinutes.setStyleSheet(u"QRadioButton::indicator::unchecked{ \n"
 "border: 1px solid; \n"
 "border-color: rgb(132,132,132);\n"
@@ -210,43 +192,84 @@ class Ui_SettingsWindow(object):
 "width: 7px; \n"
 "height: 7px; \n"
 "}")
-        self.RestoreSettings = QPushButton(SettingsWindow)
-        self.RestoreSettings.setObjectName(u"RestoreSettings")
-        self.RestoreSettings.setGeometry(QRect(280, 440, 210, 60))
-        self.RestoreSettings.setFont(font3)
-        self.RestoreSettings.setStyleSheet(u"QPushButton {\n"
-"    color: #333;\n"
-"    border: 2px solid white;\n"
-"    border-radius: 30px;\n"
-"    border-style: outset;\n"
-"    background: rgb(102,205,170);\n"
+        self.Back = QPushButton(SettingsWindow)
+        self.Back.setObjectName(u"Back")
+        self.Back.setGeometry(QRect(50, 410, 229, 50))
+        self.Back.setFont(font1)
+        self.Back.setStyleSheet(u"QPushButton {\n"
+"background: #2CB67D;\n"
+"border: 4px solid #66CDAA;\n"
+"border-radius: 21px;\n"
 "    padding: 5px;\n"
 "    }\n"
 "\n"
 "QPushButton:hover {\n"
-"    background: rgb(3, 205, 150);\n"
+"    background: #66CDAB;\n"
 "    }\n"
 "\n"
 "QPushButton:pressed {\n"
-"    border-style: inset;\n"
-"    background: rgb(70, 255, 150);\n"
+"    background: #2CB67D;\n"
 "    }\n"
 "\n"
 "QPushButton\n"
 "{\n"
-"   color:white;\n"
+" font-family: 'Montserrat';\n"
+"font-style: normal;\n"
+"font-weight: 600;\n"
+"font-size: 22px;\n"
+"line-height: 29px;\n"
+"text-align: center;\n"
+"\n"
+"color: #FFFFFE;\n"
 "}")
+        self.RestoreSettings = QPushButton(SettingsWindow)
+        self.RestoreSettings.setObjectName(u"RestoreSettings")
+        self.RestoreSettings.setGeometry(QRect(290, 410, 201, 50))
+        self.RestoreSettings.setFont(font1)
+        self.RestoreSettings.setStyleSheet(u"QPushButton {\n"
+"background: #2CB67D;\n"
+"border: 4px solid #66CDAA;\n"
+"border-radius: 21px;\n"
+"    padding: 5px;\n"
+"    }\n"
+"\n"
+"QPushButton:hover {\n"
+"    background: #66CDAB;\n"
+"    }\n"
+"\n"
+"QPushButton:pressed {\n"
+"    background: #2CB67D;\n"
+"    }\n"
+"\n"
+"QPushButton\n"
+"{\n"
+" font-family: 'Montserrat';\n"
+"font-style: normal;\n"
+"font-weight: 600;\n"
+"font-size: 22px;\n"
+"line-height: 29px;\n"
+"text-align: center;\n"
+"\n"
+"color: #FFFFFE;\n"
+"}")
+        self.frame = QFrame(SettingsWindow)
+        self.frame.setObjectName(u"frame")
+        self.frame.setGeometry(QRect(0, 0, 541, 517))
+        self.frame.setStyleSheet(u"border: 9px solid #66CDAA;")
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.frame.raise_()
         self.frame_7.raise_()
         self.frame_5.raise_()
         self.S.raise_()
         self.label.raise_()
-        self.Back.raise_()
         self.radioButtonSetGreen.raise_()
         self.radioButtonSetDark.raise_()
         self.label_2.raise_()
         self.radioButtonSetTimeMInuteThirty.raise_()
         self.radioButtonSetTimeMInute.raise_()
         self.radioButtonSetTimeTwoMinutes.raise_()
+        self.Back.raise_()
         self.RestoreSettings.raise_()
 
         self.retranslateUi(SettingsWindow)
@@ -258,13 +281,13 @@ class Ui_SettingsWindow(object):
         SettingsWindow.setWindowTitle(QCoreApplication.translate("SettingsWindow", u"Settings", None))
         self.S.setText(QCoreApplication.translate("SettingsWindow", u"Settings", None))
         self.label.setText(QCoreApplication.translate("SettingsWindow", u"Theme :", None))
-        self.Back.setText(QCoreApplication.translate("SettingsWindow", u"Back", None))
         self.radioButtonSetGreen.setText(QCoreApplication.translate("SettingsWindow", u"Green", None))
-        self.radioButtonSetDark.setText(QCoreApplication.translate("SettingsWindow", u"Dark", None))
+        self.radioButtonSetDark.setText(QCoreApplication.translate("SettingsWindow", u"Gray", None))
         self.label_2.setText(QCoreApplication.translate("SettingsWindow", u"Time :", None))
         self.radioButtonSetTimeMInuteThirty.setText(QCoreApplication.translate("SettingsWindow", u"1:30", None))
         self.radioButtonSetTimeMInute.setText(QCoreApplication.translate("SettingsWindow", u"1:00", None))
         self.radioButtonSetTimeTwoMinutes.setText(QCoreApplication.translate("SettingsWindow", u"2:00", None))
+        self.Back.setText(QCoreApplication.translate("SettingsWindow", u"Save and go back", None))
         self.RestoreSettings.setText(QCoreApplication.translate("SettingsWindow", u"Restore settings", None))
     # retranslateUi
 
